b6f824babba2dc5f945c2b580c94cb923541f7a8
citi <francesco.chiti_1996@hotmail.it>
2020-08-16 18:45:08 +0200

    16/08/2020 - Adjusting branch before merge

--- Assets/Scripts/Managers/GameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class GameManager : MonoBehaviour {
-    // Start is called before the first frame update
-    private void Start() { }
-
-    // Update is called once per frame
-    private void Update() { }
-}
