92007da5d82936d3b213f77478e71398a8cde5ad
citi <francesco.chiti_1996@hotmail.it>
2020-08-16 19:21:19 +0200

    16/08/2020 - Edited SampleScene

--- Assets/Scripts/StateMachine/Actions/ActionFactory.cs
+++ Assets/Scripts/StateMachine/Actions/ActionFactory.cs
@@ -7,12 +7,10 @@ namespace StateMachine.Actions {
         private static readonly Dictionary<Type, IAction> Actions = new Dictionary<Type, IAction>();
 
         public static IAction GetActionOfType<T>() where T : IAction, new() {
-            if (!Actions.TryGetValue(typeof(T), out var action)){
-                Actions[typeof(T)] = Activator.CreateInstance(typeof(T)) as IAction;
-                action = Actions[typeof(T)];
-            }
-
-            return action;
+            if (Actions.TryGetValue(typeof(T), out var action)) return action;
+            
+            Actions[typeof(T)] = Activator.CreateInstance(typeof(T)) as IAction;
+            return Actions[typeof(T)];
         }
     }
 }
--- Assets/Scripts/User/Inventory.cs
+++ Assets/Scripts/User/Inventory.cs
@@ -9,96 +9,91 @@ using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.UIElements;
 
-namespace User
-{
-	public class Inventory : MonoBehaviour{
-		private readonly IList<IBook> _books = new List<IBook>();
-		private readonly IList<IPlant> _plants = new List<IPlant>();
+namespace User {
+    public class Inventory : MonoBehaviour {
+        private readonly IList<IBook> _books = new List<IBook>();
+        private readonly IList<IPlant> _plants = new List<IPlant>();
 
-		public static Inventory Instance { get; private set; }
+        public static Inventory Instance { get; private set; }
 
-		// Remember to handle null SelectedBook when the last charge is used and book is deleted
-		// Something like this inside an action:
-		/*
-		SelectedBook ?? SelectedBook : (PCBasicAbility);
-		 */
-		public IBook SelectedBook { get; private set; }
+        // Remember to handle null SelectedBook when the last charge is used and book is deleted
+        // Something like this inside an action:
+        /*
+        SelectedBook ?? SelectedBook : (PCBasicAbility);
+         */
+        public IBook SelectedBook { get; private set; }
 
-		private void Awake() {
-			if (Instance == null) {
-				Instance = this;
-			} else {
-				Destroy(this);
-			}
-		}
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            } else {
+                Destroy(this);
+            }
+        }
 
-		/// <summary>
-		/// Try to add a consumable to the inventory, if it returns false then it is either an instant-use consumable
-		/// or the item list for this consumable is full
-		/// </summary>
-		/// <param name="consumable">Consumable item to be added to the inventory</param>
-		/// <returns>Returns true if item is handled correctly</returns>
-		public bool TryAddConsumableToInventory (IConsumable consumable) {
-			bool success = false;
+        /// <summary>
+        /// Try to add a consumable to the inventory, if it returns false then it is either an instant-use consumable
+        /// or the item list for this consumable is full
+        /// </summary>
+        /// <param name="consumable">Consumable item to be added to the inventory</param>
+        /// <returns>Returns true if item is handled correctly</returns>
+        public bool TryAddConsumableToInventory(IConsumable consumable) {
+            bool success = false;
 
-			if (consumable is IBook book) {
-				if (_books.Count < 3) {
-					_books.Add(book);
-					// TODO: Display "Book added to inventory"
-					success = true;
-				} else {
-					// TODO: Display "Maximum book capacity reached"
-				}
-			}
-			else if (consumable is IPlant plant) {
-				if (_plants.Count < 10) {
-					_plants.Add(plant);
-					// TODO: Display "plant added to inventory"
-					success = true;
-				} else {
-					// TODO: Display "Maximum plant capacity reached"
-				}
-			}
-			else if (consumable is IPage page) {
-				var compatibleBook = _books.FirstOrDefault(bookNotMax => bookNotMax.AddCharge(page));
-				success = compatibleBook != null;
-			}
+            if (consumable is IBook book) {
+                if (_books.Count < 3) {
+                    _books.Add(book);
+                    // TODO: Display "Book added to inventory"
+                    success = true;
+                } else {
+                    // TODO: Display "Maximum book capacity reached"
+                }
+            } else if (consumable is IPlant plant) {
+                if (_plants.Count < 10) {
+                    _plants.Add(plant);
+                    // TODO: Display "plant added to inventory"
+                    success = true;
+                } else {
+                    // TODO: Display "Maximum plant capacity reached"
+                }
+            } else if (consumable is IPage page) {
+                var compatibleBook = _books.FirstOrDefault(bookNotMax => bookNotMax.AddCharge(page));
+                success = compatibleBook != null;
+            }
 
-			return success;
-		}
+            return success;
+        }
 
-		/// <summary>
-		/// Try to remove a consumable from the inventory, if it returns false then this function has been wrongly used on an 
-		/// instant-use consumable or an error occured.
-		/// </summary>
-		/// <param name="consumable">Consumable item to be removed from the inventory</param>
-		/// <returns>Returns true if item is handled correctly</returns>
-		public bool TryRemoveConsumableFromInventory (IConsumable consumable) {
-			bool success = false;
+        /// <summary>
+        /// Try to remove a consumable from the inventory, if it returns false then this function has been wrongly used on an 
+        /// instant-use consumable or an error occured.
+        /// </summary>
+        /// <param name="consumable">Consumable item to be removed from the inventory</param>
+        /// <returns>Returns true if item is handled correctly</returns>
+        public bool TryRemoveConsumableFromInventory(IConsumable consumable) {
+            bool success = false;
 
-			if (consumable is IBook book) {
-				if (_books.Count > 0) {
-					_books.Remove(book);
-					// TODO: Display "Book destroyed"
-					success = true;
-				} else {
-					Debug.Log("Book list error");
-				}
-			} else if (consumable is IPlant plant) {
-				if (_plants.Count > 0) {
-					_plants.Remove(plant);
-					// TODO: Display "plant eated"
-					success = true;
-				} else {
-					Debug.Log("Plant list error");
-				}
-			}
+            if (consumable is IBook book) {
+                if (_books.Count > 0) {
+                    _books.Remove(book);
+                    // TODO: Display "Book destroyed"
+                    success = true;
+                } else {
+                    Debug.Log("Book list error");
+                }
+            } else if (consumable is IPlant plant) {
+                if (_plants.Count > 0) {
+                    _plants.Remove(plant);
+                    // TODO: Display "plant eated"
+                    success = true;
+                } else {
+                    Debug.Log("Plant list error");
+                }
+            }
 
-			return success;
-		}
+            return success;
+        }
 
-		private void Update() {
-
-		}
-	}
+        private void Update() { }
+    }
 }
