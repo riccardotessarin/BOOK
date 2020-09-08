6cdfdb9b48849cb34cb6ca7f1eff733f817d51dd
citi <francesco.chiti_1996@hotmail.it>
2020-09-08 23:07:03 +0200

    Add username

--- Assets/Scripts/Level/Tests/BloodBath.cs
+++ Assets/Scripts/Level/Tests/BloodBath.cs
@@ -2,33 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Characters.NPC;
-namespace Test{
-    public class BloodBath : Trial
-    {
-        [SerializeField]protected Transform[] spawnPoints=new Transform[3];
-        [SerializeField]protected CyborgKinean prefab;
-        [SerializeField]protected CyborgKinean[] enemies=new CyborgKinean[3];
-        protected override void Starter(){
+
+namespace Test {
+    public class BloodBath : Trial {
+        [SerializeField] protected Transform[] spawnPoints = new Transform[3];
+        [SerializeField] protected CyborgKinean prefab;
+        [SerializeField] protected CyborgKinean[] enemies = new CyborgKinean[3];
+
+        protected override void Starter() {
             base.Starter();
-            description="Destroy 3 Cyborg_Kineans that will spawn in this area";
-            prefab=Resources.Load<GameObject>("Prefabs/NPC/Cyborg_Kinean").GetComponent<CyborgKinean>();
+            description = "Destroy 3 Cyborg_Kineans that will spawn in this area";
+            prefab = Resources.Load<GameObject>("Prefabs/NPC/Cyborg_Kinean").GetComponent<CyborgKinean>();
         }
-        
-        public override void StartTrial(){
+
+        public override void StartTrial() {
             Debug.Log("Trial Started");
-            start=true;
-            for(int i=0;i<spawnPoints.Length;i++){
-                enemies[i]=Instantiate(prefab,spawnPoints[i]);
+            start = true;
+            for (int i = 0; i < spawnPoints.Length; i++) {
+                enemies[i] = Instantiate(prefab, spawnPoints[i]);
             }
         }
-        protected override void EndTrial(){
-            for(int i=0;i<enemies.Length;i++){
-                if(!enemies[i].IsDeath)
+
+        protected override void EndTrial() {
+            for (int i = 0; i < enemies.Length; i++) {
+                if (!enemies[i].IsDeath)
                     return;
             }
-            completed=true;
+
+            completed = true;
             Debug.Log("Trial Completed");
         }
-        
     }
 }
